db3df6f392012a0c9623e75239df5828464d5bfa
RyckoPermana <39666092+RyckoPermana@users.noreply.github.com>
2018-05-27 07:39:38 -0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,123 +1,46 @@
 <?php
-/*
-copyright @ medantechno.com
-Modified @ Farzain - zFz
-2017
-
-*/
-
 require_once('./line_class.php');
-require_once('./unirest-php-master/src/Unirest.php');
-
-$channelAccessToken = 'Ntu9HcCgwUtCnRhipkT88r4fx1h3y16m7qnLwtD7imy9z4+kztDiX14YU8Kd3xSYoV8jRe88/7eqtJnAfNMwgGA2gS/NEnLOrLpAoKFbJM2lIqUkbWvCx5SRvBg0BEqJ/zLUOqPm352MhRX0/eWQPgdB04t89/1O/w1cDnyilFU='; //sesuaikan 
-$channelSecret = '400d226b31448e7ad3d965b907532c8a';//sesuaikan
-
+$channelAccessToken = 'qTUqjN7AF75WZ1J2c2Ko0v4i31O6/LQpbcDBWuXCeXIpmpstbgSUmjIQ6aV5Oig9oV8jRe88/7eqtJnAfNMwgGA2gS/NEnLOrLpAoKFbJM3fWcsrUAO3ZIaG+Sbrf48hUUpRhhqj5d8H18uqdA+bwQdB04t89/1O/w1cDnyilFU='; //Channel access token
+$channelSecret = 'b2fc02897081519edbc1bc1733d2432f';//Channel secret
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
-
-$userId 	= $client->parseEvents()[0]['source']['userId'];
-$groupId 	= $client->parseEvents()[0]['source']['groupId'];
 $replyToken = $client->parseEvents()[0]['replyToken'];
-$timestamp	= $client->parseEvents()[0]['timestamp'];
-$type 		= $client->parseEvents()[0]['type'];
-
 $message 	= $client->parseEvents()[0]['message'];
-$messageid 	= $client->parseEvents()[0]['message']['id'];
-
-$profil = $client->profil($userId);
-
-$pesan_datang = explode(" ", $message['text']);
-
-$command = $pesan_datang[0];
-$options = $pesan_datang[1];
-if (count($pesan_datang) > 2) {
-    for ($i = 2; $i < count($pesan_datang); $i++) {
-        $options .= '+';
-        $options .= $pesan_datang[$i];
-    }
-}
-
-#-------------------------[Function]-------------------------#
-function shalat($keyword) {
-    $uri = "https://time.siswadi.com/pray/" . $keyword;
-
-    $response = Unirest\Request::get("$uri");
-
-    $json = json_decode($response->raw_body, true);
-    $result = "Jadwal Shalat Sekitar ";
-	$result .= $json['location']['address'];
-	$result .= "\nTanggal : ";
-	$result .= $json['time']['date'];
-	$result .= "\n\nShubuh : ";
-	$result .= $json['data']['Fajr'];
-	$result .= "\nDzuhur : ";
-	$result .= $json['data']['Dhuhr'];
-	$result .= "\nAshar : ";
-	$result .= $json['data']['Asr'];
-	$result .= "\nMaghrib : ";
-	$result .= $json['data']['Maghrib'];
-	$result .= "\nIsya : ";
-	$result .= $json['data']['Isha'];
-    return $result;
-}
-#-------------------------[Function]-------------------------#
-
-# require_once('./src/function/search-1.php');
-# require_once('./src/function/download.php');
-# require_once('./src/function/random.php');
-# require_once('./src/function/search-2.php');
-# require_once('./src/function/hard.php');
-
-//show menu, saat join dan command /menu
-if ($type == 'join' || $command == '/menu') {
-    $text = "Assalamualaikum Kakak, aku adalah bot jadwal shalat, silahkan ketik\n\n/shalat <nama tempat>\n\nnanti aku bakalan kasih tahu jam berapa waktunya shalat ^_^";
-    $balas = array(
-        'replyToken' => $replyToken,
+$msg_type = $message['type'];
+$botname = "KerangAjaib"; //Nama bot
+function send($input, $rt){
+    $send = array(
+        'replyToken' => $rt,
         'messages' => array(
             array(
-                'type' => 'text',
-                'text' => $text
+                'type' => 'text',					
+                'text' => $input
             )
         )
     );
+    return($send);
 }
-
-//pesan bergambar
-if($message['type']=='text') {
-	    if ($command == '/shalat') {
-
-        $result = shalat($options);
-        $balas = array(
-            'replyToken' => $replyToken,
-            'messages' => array(
-                array(
-                    'type' => 'text',
-                    'text' => $result
-                )
-            )
-        );
-    }
-
-}else if($message['type']=='sticker')
-{	
-	$balas = array(
-							'replyToken' => $replyToken,														
-							'messages' => array(
-								array(
-										'type' => 'text',									
-										'text' => 'Makasih Kak Stikernya ^_^'										
-									
-									)
-							)
-						);
-						
+function jawabs(){
+    $list_jwb = array(
+		'Ya',
+		'Tidak',
+		'Bisa jadi',
+		'Mungkin',
+		'Tentu tidak',
+		'Coba tanya lagi'
+		);
+    $jaws = array_rand($list_jwb);
+    $jawab = $list_jwb[$jaws];
+    return($jawab);
 }
-if (isset($balas)) {
-    $result = json_encode($balas);
-//$result = ob_get_clean();
-
-    file_put_contents('./balasan.json', $result);
-
-
-    $client->replyMessage($balas);
+if($msg_type == 'text'){
+    $pesan_datang = strtolower($message['text']);
+    $filter = explode(' ', $pesan_datang);
+    if($filter[0] == 'apakah') {
+        $balas = send(jawabs(), $replyToken);
+    } else {}
+} else {}
+if(isset($balas)){
+    $client->replyMessage($balas); 
+    $result =  json_encode($balas);
+    file_put_contents($botname.'.json',$result);
 }
-?>
